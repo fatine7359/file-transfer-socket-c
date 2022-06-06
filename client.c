33bb2a62e5b8ececad683e138a26071f49bbb188
hamza-boudouche <boudouche.hamza.13.07.2000@gmail.com>
2022-06-06 08:12:49 +0100

    changes

--- client.c
+++ client.c
@@ -5,62 +5,130 @@
 #include <netinet/in.h>
 
 #include <string.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <time.h>
 
-int main(int argc, char *argv[]) {
+void write_file(int sockfd, char *filename)
+{
+    int n;
+    FILE *fp;
+    char fullefilename[64] = "./client_files/";
+    strcat(fullefilename, filename);
+    char buffer[1024];
+
+    fp = fopen(fullefilename, "w");
+    while (1)
+    {
+        n = recv(sockfd, buffer, 1024, 0);
+        if (n <= 0)
+        {
+            break;
+            return;
+        }
+        fprintf(fp, "%s", buffer);
+        bzero(buffer, 1024);
+    }
+    return;
+}
+
+int main(int argc, char *argv[])
+{
     int sockfd, portno, n;
     struct sockaddr_in serv_addr;
 
     struct hostent *server;
 
     char buffer[256];
-    portno = 5001;
+    portno = 5000;
 
     // create socket and get file descriptor
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
     server = gethostbyname("127.0.0.1");
 
-    if (server == NULL) {
-        fprintf(stderr,"ERROR, no such host\n");
+    if (server == NULL)
+    {
+        fprintf(stderr, "ERROR, no such host\n");
         exit(0);
     }
 
-    bzero((char *) &serv_addr, sizeof(serv_addr));
+    bzero((char *)&serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
     serv_addr.sin_port = htons(portno);
 
-
     // connect to server with server address which is set above (serv_addr)
 
-    if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+    if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    {
         perror("ERROR while connecting");
         exit(1);
     }
 
     // inside this while loop, implement communicating with read/write or send/recv function
-    while (1) {
+    while (1)
+    {
         printf("What do you want to say? ");
-        bzero(buffer,256);
-        scanf("%s", buffer);
+        bzero(buffer, 256);
+        // scanf("%s", buffer);
+        fgets(buffer, sizeof(buffer), stdin);
 
-        n = write(sockfd,buffer,strlen(buffer));
+        n = write(sockfd, buffer, strlen(buffer));
 
-        if (n < 0){
+        if (n < 0)
+        {
             perror("ERROR while writing to socket");
             exit(1);
         }
 
-        bzero(buffer,256);
+        bzero(buffer, 256);
         n = read(sockfd, buffer, 255);
 
-        if (n < 0){
+        if (n < 0)
+        {
             perror("ERROR while reading from socket");
             exit(1);
         }
         printf("server replied: %s \n", buffer);
 
-        // escape this loop, if the server sends message "quit"
+        if (!bcmp(buffer, "Files", 5))
+        {
+            bzero(buffer, 256);
+            while (bcmp(buffer, "end", 3))
+            {
+                bzero(buffer, 256);
+                n = read(sockfd, buffer, 255);
+                if (n < 0)
+                {
+                    perror("ERROR while reading from socket");
+                    exit(1);
+                }
+                if (bcmp(buffer, "end", 3))
+                    printf("file: %s \n", buffer);
+            }
+        }
+
+        if (!bcmp(buffer, "choice:", 7))
+        {
+            bzero(buffer, 256);
+            // scanf("%s", buffer);
+            fgets(buffer, sizeof(buffer), stdin);
+            n = write(sockfd, buffer, strlen(buffer));
+
+            if (n < 0)
+            {
+                perror("ERROR while writing to socket");
+                exit(1);
+            }
+
+            struct sockaddr_in new_addr;
+            socklen_t addr_size;
+            addr_size = sizeof(new_addr);
+            int new_sock = accept(sockfd, (struct sockaddr *)&new_addr, &addr_size);
+            write_file(new_sock, buffer);
+            printf("[+]Data written in the file successfully.\n");
+        }
 
         if (!bcmp(buffer, "quit", 4))
             break;
